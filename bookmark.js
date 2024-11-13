5fedebf68a1d11a27f05a9144926095c13337549
regisbilly <kubwayoregis725>
2024-11-13 19:01:58 +0200

    bookmark

---
+++ bookmark.js
@@ -0,0 +1,61 @@
+
+const websiteNameElement = document.getElementById("websiteName");
+const websiteURLElement = document.getElementById("websiteURL");
+const bookmarksList = document.getElementById("bookmarksList");
+document.getElementById("bookmarkForm").addEventListener("submit", (event) => {
+    event.preventDefault();
+    addBookmark();
+});
+displayBookmarks();
+function addBookmark() {
+    const websiteName = websiteNameElement.value.trim();
+    const websiteURL = websiteURLElement.value.trim();
+    if (!websiteName || !websiteURL) {
+        alert("Please fill in the form");
+        return;
+    }
+    const bookmark = { name: websiteName, url: websiteURL };
+    saveBookmark(bookmark);
+    websiteNameElement.value = "";
+    websiteURLElement.value = "";
+    displayBookmarks();
+}
+function saveBookmark(bookmark) {
+    const bookmarks = JSON.parse(localStorage.getItem("bookmarks")) || [];
+    bookmarks.push(bookmark);
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+}
+function displayBookmarks() {
+    const bookmarks = JSON.parse(localStorage.getItem("bookmarks")) || [];
+    bookmarksList.innerHTML = "";
+    bookmarks.forEach((bookmark, index) => {
+        const bookmarkCard = document.createElement("div");
+        bookmarkCard.classList.add("bookmark");
+        bookmarkCard.innerHTML = `
+            <h3>${bookmark.name}</h3>
+            <a href="${bookmark.url}" target="_blank"><i class="fa fa-external-link" aria-hidden="true"></i> ${bookmark.url}</a>
+            <div class="actions">
+                <button class="edit" onclick="editBookmark(${index})">
+                    <i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit
+                </button>
+                <button class="delete" onclick="deleteBookmark(${index})">
+                    <i class="fa fa-trash" aria-hidden="true"></i> Delete
+                </button>
+            </div>
+        `;
+        bookmarksList.appendChild(bookmarkCard);
+    });
+}
+function deleteBookmark(index) {
+    const bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+    bookmarks.splice(index, 1);
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+    displayBookmarks();
+}
+function editBookmark(index) {
+    const bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+    const bookmark = bookmarks[index];
+    websiteNameElement.value = bookmark.name;
+    websiteURLElement.value = bookmark.url;
+    deleteBookmark(index);
+}
